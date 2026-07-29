b433ec78d4e207da3bd2804e3a1b9672afe1f65c
YALLICO OWNER <yallico2024@gmail.com>
2026-07-28 21:45:10 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,34 +1,19 @@
-let handler = async (m, { conn, usedPrefix, args, isAdmin }) => {
-    if (!m.isGroup) return m.reply('⚡ Este comando solo funciona en grupos')
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin }) => {
+  let isEnable = /true|on|1/i.test(command)
+  let chat = global.db.data.chats[m.chat]
+  let type = (args[0] || '').toLowerCase()
+
+  if (type == 'welcome' || type == 'bienvenida' || type == 'bv') {
+    if (!m.isGroup) return global.dfail('group', m, conn)
     if (!isAdmin) return global.dfail('admin', m, conn)
 
-    // Auto crear si no existe
-    if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
-    if (global.db.data.chats[m.chat].welcome == null) global.db.data.chats[m.chat].welcome = true
+    chat.bienvenida = isEnable
+    return m.reply(`*BIENVENIDA:* ${isEnable? 'ACTIVADA ✅' : 'DESACTIVADA ❌'}`)
+  }
 
-    if (!args[0]) return m.reply(`╭─❒ *『 ⚡ 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ 🛡️ *CONFIG WELCOME*
-│
-│ 📌 *USO:* ${usedPrefix}welcome on
-│ 📌 *USO:* ${usedPrefix}welcome off
-│
-│ *Estado:* ${global.db.data.chats[m.chat].welcome? 'ACTIVADO ✅' : 'DESACTIVADO ❌'}
-╰─────────────────❒`)
-
-    global.db.data.chats[m.chat].welcome = /true|on|1/i.test(args[0])
-    let estado = global.db.data.chats[m.chat].welcome? 'ACTIVADO ⚡' : 'DESACTIVADO ❌'
-
-    m.reply(`╭─❒ *『 ⚡ 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ 💻 *SISTEMA ACTUALIZADO*
-│
-│ 🤖 *Modulo:* Bienvenida
-│ 📊 *Estado:* ${estado}
-│ 👤 *Admin:* @${m.sender.split('@')[0]}
-│
-│ > *“Configuracion guardada”*
-╰─────────────────❒`, null, { mentions: [m.sender] })
+  // AQUI PUEDES AGREGAR MAS OPCIONES
+  return m.reply(`*OPCIONES:* welcome`)
 }
-handler.command = ['welcome']
-handler.group = true
-handler.admin = true
+
+handler.command = /^(enable|disable|on|off|1|0)$/i
 export default handler
