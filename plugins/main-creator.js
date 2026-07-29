0b6c23dbb554d1654e301662e82e33ade7babf05
YALLICO OWNER <yallico2024@gmail.com>
2026-07-28 21:05:28 -0500

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Irokz Dal ダーク;;\nFN:Irokz Dal ダーク\nORG:Irokz Dal ダーク\nTITLE:\nitem1.TEL;waid=639705083935:639705083935\nitem1.X-ABLabel:Whois ダーク⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Irokz Dal ダーク\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Whois ダーク⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Irokz Dal ダーク;;\nFN:Irokz Dal ダーク\nORG:Irokz Dal ダーク\nTITLE:\nitem1.TEL;waid=51927174369:51927174369\nitem1.X-ABLabel:Whois Yallico ダーク⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Whois Yallico ダーク\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Whois Yallicoダーク⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
